1ede2b7a59bbba83ef539dd920e613f18bda6e68
gbolo <george.bolo@gmail.com>
2018-11-29 09:03:25 -0500

    more test cases

--- backend/instancedetails_test.go
+++ backend/instancedetails_test.go
@@ -1,8 +1,10 @@
 package backend
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestLoadAwsInstanceDetailsJson(t *testing.T) {
+func TestAwsInstanceDetails(t *testing.T) {
 
 	// this should NEVER return an error
 	// if it does then the json data may be incorrect
@@ -13,5 +15,18 @@ func TestLoadAwsInstanceDetailsJson(t *testing.T) {
 	// check if instanceTypeDetailsCache was updated properly
 	if len(instanceTypeDetailsCache) < 1 {
 		t.Error("instanceTypeDetailsCache has not been updated")
+	}
+
+	// test that we can get instance type details
+	for iType, expected := range map[string]bool{
+		"t2.medium": true,
+		"c5d.4xlarge": true,
+		"p99.invalid": false,
+
+	}{
+		_, got := getInstanceTypeDetails(iType)
+		if expected != got {
+			t.Errorf("for type: %s got %v, expected %v", iType, got, expected)
+		}
 	}
 }
