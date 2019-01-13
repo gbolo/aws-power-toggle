7adc0d6f06327ed2a2b5d5b1bc431ae586d72ce4
Kyriakos Georgiou <kyriakos.a.georgiou@gmail.com>
2019-01-13 16:40:17 -0500

    Fix #41. Instance's toggle switch will go back to its previous UI state if start/stop request fails.

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -2,23 +2,31 @@ import MetadataApi from '../services/api/Metadata';
 import EnvironmentsApi from '../services/api/Environments';
 import InstancesApi from '../services/api/Instances';
 
+const errMsg = (e) => {
+  let msg = 'Unexpected error occured';
+  if (e && e.response && e.response.data) {
+    msg = e.response.data.error || msg;
+  }
+  return msg;
+};
+
 export const fetchVersion = ({ commit }) => {
   MetadataApi.getVersion()
     .then(data => commit('setVersion', data.version))
-    .catch(e => commit('setError', e.response.data.error));
+    .catch(e => commit('setError', errMsg(e)));
 };
 
 export const fetchAllEnvironmentsSummary = ({ commit }) => {
   EnvironmentsApi.fetchAllEnvironmentsSummary()
     .then(data => commit('setEnvironments', data))
-    .catch(e => commit('setError', e.response.data.error));
+    .catch(e => commit('setError', errMsg(e)));
 };
 
 export const fetchAllEnvironmentsDetails = ({ commit }) => {
   commit('setIsLoading', true);
   EnvironmentsApi.fetchAllEnvironmentsDetails()
     .then(data => commit('setEnvironments', data))
-    .catch(e => commit('setError', e.response.data.error))
+    .catch(e => commit('setError', errMsg(e)))
     .finally(() => commit('setIsLoading', false));
 };
 
@@ -26,32 +34,54 @@ export const fetchEnvironmentDetails = ({ commit }, id) => {
   commit('setEnvironmentLoading', { id, flag: true });
   EnvironmentsApi.fetchEnvironmentDetails(id)
     .then(data => commit('setEnvironment', { id, data }))
-    .catch(e => commit('setError', e.response.data.error))
+    .catch(e => commit('setError', errMsg(e)))
     .finally(() => commit('setEnvironmentLoading', { id, flag: false }));
 };
 
 export const startEnvironment = ({ dispatch, commit }, id) => {
+  commit('setEnvironmentLoading', { id, flag: true });
   EnvironmentsApi.startEnvironment(id)
     .then(() => dispatch('fetchEnvironmentDetails', id))
-    .catch(e => commit('setError', e.response.data.error));
+    .catch((e) => {
+      commit('setError', errMsg(e));
+      commit('setEnvironmentLoading', { id, flag: false });
+    });
 };
 
 export const stopEnvironment = ({ dispatch, commit }, id) => {
+  commit('setEnvironmentLoading', { id, flag: true });
   EnvironmentsApi.stopEnvironment(id)
     .then(() => dispatch('fetchEnvironmentDetails', id))
-    .catch(e => commit('setError', e.response.data.error));
+    .catch((e) => {
+      commit('setError', errMsg(e));
+      commit('setEnvironmentLoading', { id, flag: false });
+    });
 };
 
-export const startInstance = ({ dispatch, commit }, { id, envId }) => {
+export const startInstance = ({ commit }, { id, envId }) => {
+  commit('setInstanceLoading', { id, flag: true });
   InstancesApi.startInstance(id)
-    .then(() => dispatch('fetchEnvironmentDetails', envId))
-    .catch(e => commit('setError', e.response.data.error));
+    .then(() => commit('setInstanceStateStatus', { id, status: 'running', envId }))
+    .catch((e) => {
+      commit('setError', errMsg(e));
+      commit('setInstanceStateStatus', { id, status: 'stopped', envId });
+    })
+    .finally(() => {
+      commit('setInstanceLoading', { id, flag: false });
+    });
 };
 
-export const stopInstance = ({ dispatch, commit }, { id, envId }) => {
+export const stopInstance = ({ commit }, { id, envId }) => {
+  commit('setInstanceLoading', { id, flag: true });
   InstancesApi.stopInstance(id)
-    .then(() => dispatch('fetchEnvironmentDetails', envId))
-    .catch(e => commit('setError', e.response.data.error));
+    .then(() => commit('setInstanceStateStatus', { id, status: 'stopped', envId }))
+    .catch((e) => {
+      commit('setError', errMsg(e));
+      commit('setInstanceStateStatus', { id, status: 'running', envId });
+    })
+    .finally(() => {
+      commit('setInstanceLoading', { id, flag: false });
+    });
 };
 
 export const clearError = ({ commit }) => {
--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -17,6 +17,44 @@ export const setEnvironmentLoading = (state, { id, flag }) => {
   };
 };
 
+export const setInstanceLoading = (state, { id, flag }) => {
+  state.instancesLoading = {
+    ...state.instancesLoading,
+    [id]: flag,
+  };
+};
+
+export const setInstanceStateStatus = (state, { id, status, envId }) => {
+  state.environments = state.environments.map((env) => {
+    if (env.id !== envId) {
+      return env;
+    }
+
+    const newInstances = env.instances.map(
+      instance => (instance.id !== id
+        ? instance
+        : {
+          ...instance,
+          state: status,
+        }),
+    );
+
+    const numInstancesRunning = newInstances.filter(x => x.state === 'running').length;
+    const newState = numInstancesRunning === env.total_instances
+      ? 'running'
+      : numInstancesRunning === 0
+        ? 'stopped'
+        : 'mixed';
+
+    return {
+      ...env,
+      instances: newInstances,
+      running_instances: numInstancesRunning,
+      state: newState,
+    };
+  });
+};
+
 export const setError = (state, err) => {
   state.error = err;
 };
